d6099cf5932c88e731106e8cb64e0701e4491c7b
Higer23 <higeryazilim@gmail.com>
2026-02-01 18:48:56 +0100

    Delete Modules directory

--- Modules/domain_search.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-from colorama import Fore, Style
-
-def main():
-    print(f"{Fore.CYAN}Domain/Website Araştırma Modülü - Geliştirme Aşamasında{Style.RESET_ALL}")
-    input("Ana menüye dönmek için Enter'a basın...")
